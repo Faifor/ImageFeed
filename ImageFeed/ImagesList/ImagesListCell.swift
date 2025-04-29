73fcc658e9e49763d437cde81ab3ff225764b6d8
Danila <danila.fafr@gmail.com>
2025-04-29 19:18:38 +0300

    Sprint_08

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Данила Спиридонов on 29.04.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
