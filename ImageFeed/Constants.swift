13d3ace8161a260e18cd08fef141ffbfa891fc59
Danila <danila.fafr@gmail.com>
2025-08-06 12:50:37 +0300

    sprint_10

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Данила Спиридонов on 31.07.2025.
+//
+
+import UIKit
+import Foundation
+
+enum Constants {
+    static let accessKey = "Plcs8n2MT4jEz7K3z_agGtBy3kbkqf-3NI-Xq6TAhFM"
+    static let secretKey = "Ddj5GTdfsQZ0j2rIY7XXTy15oynsdoo8HxcA_wtr2D8"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
